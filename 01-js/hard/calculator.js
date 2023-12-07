df302bce31e5f78986030b90caf7658d232d34c8
Shahid Gillani <shahid5ssg@gmail.com>
2023-12-07 15:49:59 +0500

    Assignment 01-js hard done

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,45 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor(){
+    this.result=0;
+  }
+  add(n){
+    return this.result=this.result+n;
+  }
+  subtract(n){
+    return this.result=this.result-n;
+  }
+  multiply(n){
+    return this.result=this.result*n;
+  }
+  divide(n){
+    return this.result=this.result/n;
+  }
+  clear(){
+    return this.result=0;
+  }
+  getResult(){
+    return this.result;
+  }
+  calculate(str){
+    const exp=/^[0-9\+\-\*/\(\)]+$/
+    let input = str.replace(/\s/g, "");
 
+    if(!exp.test(input)){
+      return new Error('Invalid Expression');
+    }else{
+      let result=eval(input);
+      return result;
+    }
+  }
+}
+const myCal= new Calculator();
+myCal.add(5);
+myCal.divide(2);
+// myCal.clear()
+console.log(myCal.getResult());
+console.log(myCal.calculate('10 +   2 *    (   6 - (4 + 1) / 2) + 7'))
+console.log(myCal.calculate('5+abc'))
 module.exports = Calculator;
--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,24 @@
 */
 
 function countVowels(str) {
+  const vowels={
+    a:0,
+    e:0,
+    i:0,
+    o:0,
+    u:0,
+    totalVowels:0
+  }
     // Your code here
+  for(let i=0;i<str.length;i++){
+    console.log(str[i])
+    if(str[i].toLowerCase() in vowels){
+      vowels[str[i].toLowerCase()]++;
+      vowels.totalVowels=vowels.totalVowels++;
+    }
+  }
+  return vowels;
 }
+console.log(countVowels("Ihis is a string"))
 
 module.exports = countVowels;
